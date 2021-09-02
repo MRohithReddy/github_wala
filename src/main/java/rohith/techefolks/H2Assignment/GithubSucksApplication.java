0fcab7134c6b65be15e2852dcf6d88825d68d5f3
Rohith Reddy Mogili <m.rohithreddy035@gmail.com>
2021-09-02 18:42:27 +0530

    finally

---
+++ src/main/java/rohith/techefolks/H2Assignment/GithubSucksApplication.java
@@ -0,0 +1,13 @@
+package rohith.techefolks.H2Assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubSucksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubSucksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rohith/techefolks/H2Assignment/GithubSucksApplicationTests.java
@@ -0,0 +1,13 @@
+package rohith.techefolks.H2Assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubSucksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
